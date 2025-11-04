54c327333fa69d54f88a6bcf4948a11635ee617a
parsanayyara <parsanayyara@gmail.com>
2025-11-03 21:11:55 -0800

    volcano plots added

--- scripts/EnhancedPlot.R
+++ scripts/EnhancedPlot.R
@@ -27,8 +27,44 @@ res <- results(dds,
 #                 res=res, type = 'normal')
 
 #plot most basic volcano plot
+#EnhancedVolcano(res,
+#                lab = rownames(res),
+#                x = 'log2FoldChange',
+#                y = 'padj')
+
+# create custom key-value pairs for 'high', 'low', 'mid' expression by fold-change
+
+keyvals.colour <- ifelse(
+  res$log2FoldChange < -1.0, 'red',
+  ifelse(res$log2FoldChange > 1.0, 'green',
+         'grey'))
+keyvals.colour[is.na(keyvals.colour)] <- 'grey'
+names(keyvals.colour)[keyvals.colour == 'green'] <- 'high'
+names(keyvals.colour)[keyvals.colour == 'grey'] <- 'mid'
+names(keyvals.colour)[keyvals.colour == 'red'] <- 'low'
+
 EnhancedVolcano(res,
                 lab = rownames(res),
                 x = 'log2FoldChange',
-                y = 'padj')
-
+                y = 'padj',
+                #selectLab = rownames(res)[which(names(keyvals.colour) %in% c('High', 'Low'))],
+                xlab = bquote(~Log[2]~ 'fold change'),
+                axisLabSize = 10,
+                title = 'Control vs IL-13',
+                pCutoff = 10e-5,
+                FCcutoff = 1.0,
+                pointSize = 1.5,
+                labSize = 3.0,
+                colCustom = keyvals.colour,
+                colAlpha = 1,
+                legendPosition = 'right',
+                legendLabSize = 10,
+                legendIconSize = 3.0,
+                drawConnectors = TRUE,
+                widthConnectors = 0.5,
+                colConnectors = 'black',
+                gridlines.major = TRUE,
+                gridlines.minor = FALSE,
+                border = 'full',
+                borderWidth = 1.0,
+                borderColour = 'black')
