29ec98f693aaa87941e5f07f7ccabe61b9a0b0b6
alexmillett20 <amillett@student.ubc.ca>
2025-11-13 13:07:12 -0800

    DESeq objects for ctrl vs IL4, IL13 vs IL4

--- scripts/DEA_script_DESeq2.R
+++ scripts/DEA_script_DESeq2.R
@@ -8,8 +8,6 @@
 
 library(tidyverse)
 library(DESeq2)
-library(pheatmap)
-library(RColorBrewer)
 
 
 # Set path to a directory with files and load files into R, can do setwd("YourPath/")
@@ -195,13 +193,6 @@ combined_data_no_ctrlrep8 <- data.frame(row.names = control_rep_1$gene_id,
                             control_rep_6 = control_rep_6$total,
                             control_rep_7 = control_rep_7$total,
                             control_rep_9 = control_rep_9$total,
-                            IL4_rep_2 = IL4_rep_2$total,
-                            IL4_rep_3 = IL4_rep_3$total,
-                            IL4_rep_4 = IL4_rep_4$total,
-                            IL4_rep_5 = IL4_rep_5$total,
-                            IL4_rep_6 = IL4_rep_6$total,
-                            IL4_rep_8 = IL4_rep_8$total,
-                            IL4_rep_9 = IL4_rep_9$total,
                             IL13_rep_1 = IL13_rep_1$total,
                             IL13_rep_2 = IL13_rep_2$total,
                             IL13_rep_3 = IL13_rep_3$total,
@@ -268,8 +259,7 @@ combined_data_matrix_no_ctrlrep8_IL13 <- as.matrix(combined_data_no_ctrlrep8_IL1
 metadata_no_ctrlrep8_IL13 <- data.frame(row.names = colnames(combined_data_matrix_no_ctrlrep8_IL13), 
                                    treatment = c("control", "control", "control", "control", "control", "control",
                                                  "IL13", "IL13", "IL13", "IL13", "IL13", "IL13", "IL13", "IL13", "IL13"
-                                   )
-)
+                                   ))
 
 
 metadata_no_ctrlrep8_IL13$label <- rownames(metadata_no_ctrlrep8_IL13)
@@ -288,6 +278,99 @@ dds_matrix_no_ctrlrep8_IL13$treatment <- relevel(dds_matrix_no_ctrlrep8_IL13$tre
 
 dds_no_ctrlrep8_IL13 <- DESeq(dds_matrix_no_ctrlrep8_IL13)
 saveRDS(dds_no_ctrlrep8_IL13, "./DEA_outputs/dds_no_ctrlrep8_IL13.rds")
+
+
+## dds without control rep 8 compared to IL4 -----------------------------
+combined_data_no_ctrlrep8_IL4 <- data.frame(row.names = control_rep_1$gene_id,
+                                             control_rep_1 = control_rep_1$total,
+                                             control_rep_3 = control_rep_3$total,
+                                             control_rep_4 = control_rep_4$total,
+                                             control_rep_6 = control_rep_6$total,
+                                             control_rep_7 = control_rep_7$total,
+                                             control_rep_9 = control_rep_9$total,
+                                             IL4_rep_2 = IL4_rep_2$total,
+                                             IL4_rep_3 = IL4_rep_3$total,
+                                             IL4_rep_4 = IL4_rep_4$total,
+                                             IL4_rep_5 = IL4_rep_5$total,
+                                             IL4_rep_6 = IL4_rep_6$total,
+                                             IL4_rep_8 = IL4_rep_8$total,
+                                             IL4_rep_9 = IL4_rep_9$total)
+
+
+# transform to matrix
+combined_data_matrix_no_ctrlrep8_IL4 <- as.matrix(combined_data_no_ctrlrep8_IL4) 
+
+# Metadata for samples
+metadata_no_ctrlrep8_IL4 <- data.frame(row.names = colnames(combined_data_matrix_no_ctrlrep8_IL4), 
+                                        treatment = c("control", "control", "control", "control", "control", "control",
+                                                      "IL4", "IL4", "IL4", "IL4", "IL4", "IL4", "IL4"
+                                        ))
+
+
+metadata_no_ctrlrep8_IL4$label <- rownames(metadata_no_ctrlrep8_IL4)
+
+colnames(combined_data_matrix_no_ctrlrep8_IL4) == rownames(metadata_no_ctrlrep8_IL4)
+
+# create dds_matrix
+dds_matrix_no_ctrlrep8_IL4 <- DESeqDataSetFromMatrix(countData = combined_data_matrix_no_ctrlrep8_IL4,  
+                                                      colData = metadata_no_ctrlrep8_IL4, 
+                                                      design = ~treatment)
+
+
+# Set control
+dds_matrix_no_ctrlrep8_IL4$treatment <- relevel(dds_matrix_no_ctrlrep8_IL4$treatment, ref = "control")
+
+
+dds_no_ctrlrep8_IL4 <- DESeq(dds_matrix_no_ctrlrep8_IL4)
+saveRDS(dds_no_ctrlrep8_IL4, "./DEA_outputs/dds_no_ctrlrep8_IL4.rds")
+
+## dds IL13 compared to IL4 -----------------------------
+combined_data_IL13_IL4 <- data.frame(row.names = IL13_rep_1$gene_id,
+                                            IL13_rep_1$total,
+                                            IL13_rep_2 = IL13_rep_2$total,
+                                            IL13_rep_3 = IL13_rep_3$total,
+                                            IL13_rep_4 = IL13_rep_4$total,
+                                            IL13_rep_5 = IL13_rep_5$total,
+                                            IL13_rep_6 = IL13_rep_6$total,
+                                            IL13_rep_7 = IL13_rep_7$total,
+                                            IL13_rep_8 = IL13_rep_8$total,
+                                            IL13_rep_9 = IL13_rep_9$total,
+                                            IL4_rep_2 = IL4_rep_2$total,
+                                            IL4_rep_3 = IL4_rep_3$total,
+                                            IL4_rep_4 = IL4_rep_4$total,
+                                            IL4_rep_5 = IL4_rep_5$total,
+                                            IL4_rep_6 = IL4_rep_6$total,
+                                            IL4_rep_8 = IL4_rep_8$total,
+                                            IL4_rep_9 = IL4_rep_9$total)
+
+
+# transform to matrix
+combined_data_matrix_IL13_IL4 <- as.matrix(combined_data_IL13_IL4) 
+
+# Metadata for samples
+metadata_IL13_IL4 <- data.frame(row.names = colnames(combined_data_matrix_IL13_IL4), 
+                                       treatment = c("control", "control", "control", "control", "control", "control",
+                                                     "IL4", "IL4", "IL4", "IL4", "IL4", "IL4", "IL4"
+                                       ))
+
+
+metadata_IL13_IL4$label <- rownames(metadata_IL13_IL4)
+
+colnames(combined_data_matrix_IL13_IL4) == rownames(metadata_IL13_IL4)
+
+# create dds_matrix
+dds_matrix_IL13_IL4 <- DESeqDataSetFromMatrix(countData = combined_data_matrix_IL13_IL4,  
+                                                     colData = metadata_IL13_IL4, 
+                                                     design = ~treatment)
+
+
+# Set control
+dds_matrix_IL13_IL4$treatment <- relevel(dds_matrix_IL13_IL4$treatment, ref = "IL13")
+
+
+dds_no_IL13_IL4 <- DESeq(dds_matrix_IL13_IL4)
+saveRDS(dds_IL13_IL4, "./DEA_outputs/dds_IL13_IL4.rds")
+
 
 
 ##### generating plots -----------------------------------------------
@@ -320,78 +403,21 @@ PCA_plot_no_ctrlrep8_IL13 <- plotPCA(rld_no_ctrlrep8_IL13, intgroup = "treatment
 ggsave("./DEA_outputs/PCA_plot_no_ctrlrep8_IL13.png", PCA_plot_no_ctrlrep8_IL13, width = 10, height = 10)
 
 
-# pcaData <- plotPCA(rld, intgroup = c("treatment", "label"), returnData = TRUE)
-# 
-# percentVar <- round(100 * attr(pcaData, "percentVar"))
-# 
-# ggplot(pcaData, aes(PC1, PC2, color = treatment)) +
-#   geom_point(size = 4) +
-#   geom_text(aes(label = label), vjust = -1, size = 3) +
-#   xlab(paste0("PC1: ", percentVar[1], "% variance")) +
-#   ylab(paste0("PC2: ", percentVar[2], "% variance")) +
-#   ggtitle("PCA of rlog-transformed counts") +
-#   theme_minimal()
+##### no control rep 8 and IL4 only
+rld_no_ctrlrep8_IL4 <- rlog(dds_no_ctrlrep8_IL4)
+
+# Generate a PCA plot with DESeq2's plotPCA function
+PCA_plot_no_ctrlrep8_IL4 <- plotPCA(rld_no_ctrlrep8_IL4, intgroup = "treatment") +
+  geom_text(aes(label = label))
+ggsave("./DEA_outputs/PCA_plot_no_ctrlrep8_IL4.png", PCA_plot_no_ctrlrep8_IL4, width = 10, height = 10)
+
+#### IL13 and IL4 only
+rld_IL13_IL4 <- rlog(dds_IL13_IL4)
+
+# Generate a PCA plot with DESeq2's plotPCA function
+PCA_plot_IL13_IL4 <- plotPCA(rld_IL13_IL4, intgroup = "treatment") +
+  geom_text(aes(label = label))
+ggsave("./DEA_outputs/PCA_plot_IL13_IL4.png", PCA_plot_IL13_IL4, width = 10, height = 10)
 
 
-# Calculate distances between samples in our log-transformed data
-sample_dists <- dist(t(assay(rld)))
 
-# Convert the output to a matrix
-sample_dist_matrix <- as.matrix(sample_dists)
-
-# Remove the column names of our matrix
-colnames(sample_dist_matrix) <- NULL
-
-# Set the colour palette for our heatmap
-colours <- colorRampPalette(rev(brewer.pal(9, "Blues")))(255)
-
-# Generate a heatmap using the pheatmap package
-pheatmap(sample_dist_matrix,
-         clustering_distance_rows = sample_dists,
-         clustering_distance_cols = sample_dists, 
-         col = colours)
-
-# Names of the results that DESeq2 calculated
-resultsNames(dds)
-
-# Now we will extract the results for our comparison between the 12h timepoint and the 1h timepoint
-res <- results(dds, name = "condition_12_hour_vs_1_hour") %>% as.data.frame() # we save it as a dataframe for easy manipulation with dplyr 
-head(res)
-
-results(dds, contrast = c("condition", "12h", "6h"))
-
-glimpse(res)
-
-view(res)
-
-res_no_NA <- res %>% 
-  drop_na()
-
-# How many rows did we filter out!?
-glimpse(res_no_NA) 
-
-res_filtered <- res_no_NA %>% 
-  filter(padj <= 0.05)
-
-# How many rows did we filter out!?
-glimpse(res_filtered)
-
-
-res_filtered_final <- res_filtered %>% 
-  filter(log2FoldChange <= -1 | log2FoldChange >= 1) %>% # the '|' stand for OR here!
-  rownames_to_column("gene_id") # Convert the rownames into a column so they can be saved in your CSV file
-
-head(res_filtered_final)
-
-# Top 10 upregulated genes (most positive log2FoldChange)
-top10_genes <- res_filtered_final %>%
-  arrange(desc(log2FoldChange)) %>% # NOTE that we use the desc() function to organize the column in descending order
-  head(n = 10)
-top10_genes
-
-bot10_genes <- res_filtered_final %>%
-  arrange(log2FoldChange) %>% # NOTE since we don't use desc(), the column is organized in ascending order
-  head(n = 10)
-bot10_genes
-
-write_csv(res_filtered_final, "chlamy_results.csv")
